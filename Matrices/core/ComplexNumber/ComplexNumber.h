74c331c2e6977d46a1f53232c1c929cca87fe905
Liohki Andrey <legkij05@mail.ru>
2025-07-09 03:19:10 +0300

    restructured and refactored

--- Matrices/core/ComplexNumber/ComplexNumber.h
+++ Matrices/core/ComplexNumber/ComplexNumber.h
@@ -1,38 +1,107 @@
 #pragma once
 
 #include <iostream>
+#include <ostream>
 
-namespace core {
+namespace LineareAlgebra::Core {
 
+	template<typename T = double>
 	class ComplexNumber {
 	private:
-		static constexpr double EPSILON = 1e-12;
-		double re;
-		double im;
+
+		static_assert(
+			std::is_floating_point_v<T>,
+			"ComplexNumber<T> requires T to be a floating-point type"
+		);
+
+		T real_;
+		T imag_;
+
 	public:
-		ComplexNumber(double re = 0, double im = 0) : re(re), im(im) {};
 
-		double getRe() const;
-		double getIm() const;
-		double getAbsoluteValue() const;
+		constexpr ComplexNumber() noexcept:real_(0), imag_(0) {}
+		constexpr ComplexNumber(T real, T imag = 0) noexcept: real_(real), imag_(imag) {}
+		constexpr ComplexNumber(const std::complex<T>& complexNumber) noexcept : real_(complexNumber.real()), imag_(complexNumber.imag()) {}
+		constexpr ComplexNumber(const ComplexNumber&) noexcept = default;
+		constexpr ComplexNumber(ComplexNumber&&) noexcept = default;
 
-		ComplexNumber operator+(const ComplexNumber& other) const;
-		ComplexNumber operator-(const ComplexNumber& other) const;
-		ComplexNumber operator*(const ComplexNumber& other) const;
-		ComplexNumber operator/(const ComplexNumber& other) const;
-		ComplexNumber& operator=(const ComplexNumber& other);
-		ComplexNumber& operator=(const double& other);
+		constexpr ComplexNumber& operator=(const ComplexNumber&) noexcept = default;
+		constexpr ComplexNumber& operator=(ComplexNumber&&) noexcept = default;
 
-		bool operator==(const ComplexNumber& other) const;
-		bool operator!=(const ComplexNumber& other) const;
+		~ComplexNumber() = default;
 
-		friend std::ostream& operator<<(std::ostream& os, const ComplexNumber& c);
+		[[nodiscard]]constexpr T real() const noexcept { return real_;}
+		[[nodiscard]] constexpr T imag() const noexcept { return imag_; }
+		constexpr void real(T value) noexcept { real_ = value; }
+		constexpr void imag(T value) noexcept { imag_ = value; }
+
+		constexpr ComplexNumber& operator+=(const ComplexNumber& other) noexcept {
+			real_ += other.real_;
+			imag_ += other.imag_;
+
+			return *this;
+		}
+		constexpr ComplexNumber& operator-=(const ComplexNumber& other) noexcept {
+			real_ -= other.real_;
+			imag_ -= other.imag_;
+
+			return *this;
+		}
+		constexpr ComplexNumber& operator*=(const ComplexNumber& other) noexcept {
+			real_ = real_ * other.real_ - imag_ * other.imag_;
+			imag_ = real_ * other.imag_ + imag_ * other.real_;
+
+			return *this;
+		}
+		constexpr ComplexNumber& operator/=(const ComplexNumber& other) {
+			if (other.real_ == 0 && other.imag_ == 0) {
+				throw std::domain_error("Division by zero");
+			}
+			T denom = other.real_ * other.real_ + other.imag_ * other.imag_;
+			T new_real = (real_ * other.real_ + imag_ * other.imag_) / denom;
+			T new_imag = (imag_ * other.real_ - real_ * other.imag_) / denom;
+			real_ = new_real;
+			imag_ = new_imag;
+			return *this;
+		}
+		[[nodiscard]] constexpr ComplexNumber operator+() const noexcept { return *this; }
+		[[nodiscard]] constexpr ComplexNumber operator-() const noexcept { return { -real_, -imag_ }; }
+		[[nodiscard]] friend constexpr ComplexNumber operator+(ComplexNumber a, const ComplexNumber& b) noexcept {
+			return a += b;
+		}
+		[[nodiscard]] friend constexpr ComplexNumber operator-(ComplexNumber a, const ComplexNumber& b) noexcept {
+			return a -= b;
+		}
+		[[nodiscard]] friend constexpr ComplexNumber operator*(ComplexNumber a, const ComplexNumber& b) noexcept {
+			return a *= b;
+		}
+		[[nodiscard]] friend constexpr ComplexNumber operator/(ComplexNumber a, const ComplexNumber& b) {
+			return a /= b;  
+		}
+		friend constexpr bool operator==(const ComplexNumber& a, const ComplexNumber& b) noexcept {
+			return (a.real() == b.real() && a.imag() == b.imag())
+				|| (std::isnan(a.real()) && std::isnan(b.real()))
+				|| (std::isnan(a.imag()) && std::isnan(b.imag()));
+		}
+		friend constexpr bool operator!=(const ComplexNumber& a, const ComplexNumber& b) noexcept {
+			return !(a == b);
+		}
+		friend std::ostream& operator<<(std::ostream& os, const ComplexNumber& z) {
+			return os << z.real() << (z.imag() >= 0 ? "+" : "") << z.imag() << "i";
+		}
+	
+		[[nodiscard]] constexpr T abs() const noexcept {
+			return std::hypot(real_, imag_);
+		}
+		[[nodiscard]] constexpr T arg() const noexcept {
+			return std::atan2(imag_, real_);
+		}
+		[[nodiscard]] constexpr ComplexNumber conj() const noexcept {
+			return { real_, -imag_ };
+		}
+		[[nodiscard]] constexpr bool is_zero() const noexcept {
+			return real_ == 0 && imag_ == 0;
+		}
 	};
 
-	ComplexNumber getConjugated(const ComplexNumber& number) {
-		core::ComplexNumber conjugatedNumber(number.getRe(), -1 * number.getIm());
-		return conjugatedNumber;
-	}
-
-}//namespace Core
-
+}//namespace LineareAlgebra::Core
--- Matrices/core/Matrix/Matrix.h
+++ Matrices/core/Matrix/Matrix.h
@@ -1,29 +1,151 @@
 #pragma once
 
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 #include "ComplexNumber.h"
 
-namespace core {
+namespace LineareAlgebra::Core {
 
+	template<typename T>
 	class Matrix {
 	private:
-		std::vector<std::vector<ComplexNumber>> matrix;
-		std::size_t rows;
-		std::size_t columns;
-	public:
-		Matrix(const std::vector<std::vector<ComplexNumber>>& mat);
-		Matrix(const std::size_t& rows, const std::size_t& columns);
-		Matrix(const std::size_t& rows, const std::size_t& columns, const ComplexNumber& value);
 
-		std::size_t getRows() const;
-		std::size_t getColumns() const;
+		static_assert(
+			std::is_floating_point_v<T> ||
+			std::is_same_v<T, ComplexNumber<typename T::value_type>>,
+			"Matrix<T> requires T to be either a floating-point type or ComplexNumber"
+		);
 
-		ComplexNumber& operator()(const std::size_t& row, const std::size_t& columns);
-		const ComplexNumber& operator()(const std::size_t& row, const std::size_t& column) const;
+		std::vector<std::vector<T>> data_;
+		size_t rows_;
+		size_t columns_;
 
-		friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
-	};
 
-}//namespace Core
+        void check_rectangular() const {
+            for (const auto& row : data_) {
+                if (row.size() != columns_) {
+                    throw std::invalid_argument("All rows must have the same length");
+                }
+            }
+        }
+        void check_dimensions(const Matrix& other) const {
+            if (rows_ != other.rows_ || columns_ != other.columns_) {
+                throw std::invalid_argument("Matrix dimensions must agree");
+            }
+        }
+    
+    public:
+
+        Matrix() noexcept : rows_(0), columns_(0) {}
+        Matrix(size_t rows, size_t columns)
+            noexcept(std::is_nothrow_constructible_v<std::vector<T>>)
+            : data_(rows, std::vector<T>(columns)),
+            rows_(rows),
+            columns_(columns) {}
+        Matrix(size_t rows, size_t columns, const T& value)
+            noexcept(std::is_nothrow_copy_constructible_v<T>)
+            : data_(rows, std::vector<T>(columns, value)),
+            rows_(rows),
+            columns_(columns) {}
+        explicit Matrix(const std::vector<std::vector<T>>& data)
+            : data_(data),
+            rows_(data.size()),
+            columns_(data.empty() ? 0 : data[0].size())
+        {
+            check_rectangular();
+        }
+        Matrix(const Matrix& other) = default;
+        Matrix(Matrix&& other) noexcept = default;
+
+        Matrix& operator=(const Matrix& other) = default;
+        Matrix& operator=(Matrix&& other) noexcept = default;
+
+        [[nodiscard]] size_t rows() const noexcept { return rows_; }
+        [[nodiscard]] size_t columns() const noexcept { return columns_; }
+        [[nodiscard]] const std::vector<std::vector<T>>& data() const noexcept { return data_; }
+	
+        [[nodiscard]] friend Matrix operator+(const Matrix& lhs, const Matrix& rhs) {
+            lhs.check_dimensions(rhs);
+            Matrix result(lhs.rows_, lhs.columns_);
+            for (size_t i = 0; i < lhs.rows_; ++i) {
+                for (size_t j = 0; j < lhs.columns_; ++j) {
+                    result.data_[i][j] = lhs.data_[i][j] + rhs.data_[i][j];
+                }
+            }
+            return result;
+        }
+        [[nodiscard]] friend Matrix operator-(const Matrix& lhs, const Matrix& rhs) {
+            lhs.check_dimensions(rhs);
+            Matrix result(lhs.rows_, lhs.columns_);
+            for (size_t i = 0; i < lhs.rows_; ++i) {
+                for (size_t j = 0; j < lhs.columns_; ++j) {
+                    result.data_[i][j] = lhs.data_[i][j] - rhs.data_[i][j];
+                }
+            }
+            return result;
+        }
+        [[nodiscard]] friend Matrix operator*(const Matrix& lhs, const Matrix& rhs) {
+            if (lhs.columns_ != rhs.rows_) {
+                throw std::invalid_argument("Incompatible matrix dimensions for multiplication");
+            }
+            Matrix result(lhs.rows_, rhs.columns_);
+            for (size_t i = 0; i < lhs.rows_; ++i) {
+                for (size_t j = 0; j < rhs.columns_; ++j) {
+                    for (size_t k = 0; k < lhs.columns_; ++k) {
+                        result.data_[i][j] += lhs.data_[i][k] * rhs.data_[k][j];
+                    }
+                }
+            }
+            return result;
+        }
+        [[nodiscard]] friend Matrix operator*(const Matrix& matrix, T scalar) {
+            Matrix result(matrix.rows_, matrix.columns_);
+            for (size_t i = 0; i < matrix.rows_; ++i) {
+                for (size_t j = 0; j < matrix.columns_; ++j) {
+                    result.data_[i][j] = matrix.data_[i][j] * scalar;
+                }
+            }
+            return result;
+        }
+        [[nodiscard]] friend Matrix operator*(T scalar, const Matrix& matrix) {
+            return matrix * scalar;
+        }
+        friend bool operator==(const Matrix& lhs, const Matrix& rhs) {
+            return lhs.data_ == rhs.data_;
+        }
+        friend bool operator!=(const Matrix& lhs, const Matrix& rhs) {
+            return !(lhs == rhs);
+        }
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+            for (const auto& row : matrix.data_) {
+                for (const auto& elem : row) {
+                    os << elem << ' ';
+                }
+                os << '\n';
+            }
+            return os;
+        }
+
+        T& operator()(size_t i, size_t j) { return data_[i][j]; }
+        const T& operator()(size_t i, size_t j) const { return data_[i][j]; }
+        Matrix& operator+=(const Matrix& other) {
+            check_dimensions(other);
+            for (size_t i = 0; i < rows_; ++i) {
+                for (size_t j = 0; j < columns_; ++j) {
+                    data_[i][j] += other.data_[i][j];
+                }
+            }
+            return *this;
+        }
+        Matrix& operator-=(const Matrix& other) {
+            check_dimensions(other);
+            for (size_t i = 0; i < rows_; ++i) {
+                for (size_t j = 0; j < columns_; ++j) {
+                    data_[i][j] -= other.data_[i][j];
+                }
+            }
+            return *this;
+        }
+    };
+}
