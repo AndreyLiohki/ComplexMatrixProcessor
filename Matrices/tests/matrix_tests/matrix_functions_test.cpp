117a6db6434b95d137e525661423709da32dd8f1
Liohki Andrey <legkij05@mail.ru>
2025-07-31 21:39:00 +0300

    test for column

--- Matrices/tests/matrix_tests/matrix_functions_test.cpp
+++ Matrices/tests/matrix_tests/matrix_functions_test.cpp
@@ -96,4 +96,65 @@ namespace {
         EXPECT_THROW(m3x3.swap_rows(3, 0), std::out_of_range);
         EXPECT_THROW(m3x3.swap_rows(3, 3), std::out_of_range);
     }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnReturnsCorrectValues) {
+        Matrix<double> m(3, 3);
+        m(0) = { 1.0, 2.0, 3.0 };
+        m(1) = { 4.0, 5.0, 6.0 };
+        m(2) = { 7.0, 8.0, 9.0 };
+
+        auto col1 = m.getColumn(0);
+        ASSERT_EQ(col1.size(), 3);
+        EXPECT_NEAR(col1[0], 1.0, epsilon);
+        EXPECT_NEAR(col1[1], 4.0, epsilon);
+        EXPECT_NEAR(col1[2], 7.0, epsilon);
+
+        const Matrix<double>& cm = m;
+        auto col2 = cm.getColumn(1);
+        ASSERT_EQ(col2.size(), 3);
+        EXPECT_NEAR(col2[0], 2.0, epsilon);
+        EXPECT_NEAR(col2[1], 5.0, epsilon);
+        EXPECT_NEAR(col2[2], 8.0, epsilon);
+    }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnWorksWithEmptyMatrix) {
+        Matrix<double> empty;
+        EXPECT_THROW(empty.getColumn(0), std::out_of_range);
+    }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnThrowsForInvalidIndex) {
+        Matrix<double> m(2, 2);
+        EXPECT_THROW(m.getColumn(2), std::out_of_range);
+        EXPECT_THROW(m.getColumn(-1), std::out_of_range);
+    }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnReturnsCorrectVectorSize) {
+        Matrix<double> m(4, 2);
+        auto col = m.getColumn(1);
+        EXPECT_EQ(col.size(), 4);
+    }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnDoesNotModifyOriginalMatrix) {
+        Matrix<double> m(2, 2);
+        m(0) = { 1.0, 2.0 };
+        m(1) = { 3.0, 4.0 };
+
+        auto original = m;
+        auto col = m.getColumn(1);
+        col[0] = 5.0;
+
+        EXPECT_NEAR(m(0, 1), 2.0, epsilon);
+        EXPECT_NEAR(m(1, 1), 4.0, epsilon);
+    }
+
+    TEST_F(MatrixUtilityFunctionsTest, GetColumnWorksWithDifferentValueTypes) {
+        Matrix<double> int_m(2, 2);
+        int_m(0) = { 1, 2 };
+        int_m(1) = { 3, 4 };
+
+        auto int_col = int_m.getColumn(0);
+        ASSERT_EQ(int_col.size(), 2);
+        EXPECT_EQ(int_col[0], 1);
+        EXPECT_EQ(int_col[1], 3);
+    }
 }
