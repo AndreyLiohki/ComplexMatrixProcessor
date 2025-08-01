12f4fdf6ebf569c757996d2f060dbc76fa1d61df
Liohki Andrey <legkij05@mail.ru>
2025-08-01 14:15:54 +0300

    added identity vector creation

--- Matrices/include/matrixlib/decompositions/qr_decomposition.h
+++ Matrices/include/matrixlib/decompositions/qr_decomposition.h
@@ -46,34 +46,41 @@ namespace Decompositions {
 				T alpha = std::sqrt(compute_scalar_product(choosed_column, choosed_column));
 			}
 
-			template<typename T>
-			T scalar_product(const std::vector<T>& vec1, const std::vector<T>& vec2, bool conjugate_first = true) {
+			T scalar_product(const std::vector<T>& vector_1, const std::vector<T>& vector_2, bool conjugate_first = true) {
 				static_assert(is_valid_matrix_type<T>::value, "Unsupported type");
 
-				if (vec1.size() != vec2.size()) {
+				if (vector_1.size() != vector_2.size()) {
 					throw std::invalid_argument("Vectors must have the same size");
 				}
 
 				T result{ 0 };
 				if constexpr (is_complex<T>::value) {
 					if (conjugate_first) {
-						for (size_t i = 0; i < vec1.size(); ++i) {
-							result += std::conj(vec1[i]) * vec2[i];
+						for (size_t i = 0; i < vector_1.size(); ++i) {
+							result += std::conj(vector_1[i]) * vector_2[i];
 						}
 					}
 					else {
-						for (size_t i = 0; i < vec1.size(); ++i) {
-							result += vec1[i] * vec2[i];
+						for (size_t i = 0; i < vector_1.size(); ++i) {
+							result += vector_1[i] * vector_2[i];
 						}
 					}
 				}
 				else {
-					for (size_t i = 0; i < vec1.size(); ++i) {
-						result += vec1[i] * vec2[i];
+					for (size_t i = 0; i < vector_1.size(); ++i) {
+						result += vector_1[i] * vector_2[i];
 					}
 				}
 				return result;
 			}
+		
+			std::vector<T> get_identity_vector(const size_t size_of_vector, const size_t position_of_1s) {
+				std::vector<T> result(size_of_vector, T{ 0 });
+				result[position_of_1s] = T{ 1 };
+				return result;
+			}
+
+			std::vector<T> vector_summation(const )
 		};
 	}
 }
